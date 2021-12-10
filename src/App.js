51a8a8552095f339e81dedc469b9904860918dfd
Igor <polygor@gmail.com>
2021-12-10 15:23:05 +0200

    11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <h1>TEST</h1>
+    </div>
+  );
 }
 
 export default App;
